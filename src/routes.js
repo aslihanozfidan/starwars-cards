ebbdfeb851ea94c6cf98ecba9c1126d2e9f88e88
aslihanozfidan <aslihanozfidan@gmail.com>
2018-10-01 00:19:16 +0300

    add new sub routes

--- src/routes.js
+++ src/routes.js
@@ -5,15 +5,22 @@ import PlanetsCards from './containers/PlanetsCards';
 import StarshipsCards from './containers/StarshipsCards';
 import VehiclesCards from './containers/VehiclesCards';
 import SpeciesCards from './containers/SpeciesCards';
+import CardDetails from './containers/CardDetails';
 
 const Routes = () => (
   <main>
     <Switch>
-      <Route exact path='/characters' component={CharacterCards}/>
+      <Route exact path='/people' component={CharacterCards}/>
       <Route exact path='/planets' component={PlanetsCards}/>
       <Route exact path='/starships' component={StarshipsCards}/>
       <Route exact path='/vehicles' component={VehiclesCards}/>
       <Route exact path='/species' component={SpeciesCards}/>
+
+      <Route path="/people/:id" component={CardDetails}/>
+      <Route path="/planets/:id" component={CardDetails}/>
+      <Route path="/starships/:id" component={CardDetails}/>
+      <Route path="/vehicles/:id" component={CardDetails}/>
+      <Route path="/species/:id" component={CardDetails}/>
     </Switch>
   </main>
 )
