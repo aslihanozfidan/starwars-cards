89657169cc380d28952a8278c0a75b43c182cd48
aslihanozfidan <aslihanozfidan@gmail.com>
2018-10-01 00:17:43 +0300

    fixed api dir name and running script

--- src/api/main.go
+++ src/api/main.go
@@ -5,8 +5,9 @@ package main
 	"net/http"
 )
  func main() {
+	println("Api is running")
 	flag.Parse()
- 	fs := http.FileServer(http.Dir("photos"))
+ 	fs := http.FileServer(http.Dir("src/api/photos"))
 	http.Handle("/", fs)
 	log.Fatal(http.ListenAndServe(":5000", nil))
 } 
